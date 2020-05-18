fcb12f998121c1f40032f479d18afb0b944d3a2b
pduren <57735971+kParkz@users.noreply.github.com>
2020-05-18 13:35:41 -0500

    finished everything besides stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  const params = useParams();
+
   useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
+    axios
+      .get(`http://localhost:5000/api/movies/${params.id}`)
+      .then((response) => {
+        setMovie(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [params.id]);
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-
-  },[]);
-  
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -43,7 +40,7 @@ const Movie = (props) => {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars.map((star) => (
           <div key={star} className="movie-star">
             {star}
           </div>
@@ -52,6 +49,6 @@ const Movie = (props) => {
       <div className="save-button">Save</div>
     </div>
   );
-}
+};
 
 export default Movie;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-const MovieList = props => {
+const MovieList = (props) => {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.movies.map((movie) => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
+  const history = useHistory();
+
+  function handleClick() {
+    history.push(`/movies/${movie.id}`);
+  }
+
   return (
-    <div className="movie-card">
+    <div onClick={handleClick} className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -23,7 +30,7 @@ function MovieDetails({ movie }) {
       </div>
       <h3>Actors</h3>
 
-      {stars.map(star => (
+      {stars.map((star) => (
         <div key={star} className="movie-star">
           {star}
         </div>
